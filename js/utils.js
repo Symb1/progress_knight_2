cd27aa4c277bea3970ea7fdb1f98d82740df9fc6
James103 <37945304+James103@users.noreply.github.com>
2022-12-18 07:07:21 -0600

    Made formatTime better and add formatWhole
    formatTime now supports long time durations, up to 1000 years at full precision, with longer times in compact notation.

--- js/utils.js
+++ js/utils.js
@@ -33,6 +33,13 @@ function format(number, decimals = 1) {
     }
 }
 
+function formatWhole(number, decimals = 1) {
+    if (number >= 1e3 || (number <= 0.99 && number != 0)) {
+        return format(number, decimals)
+    }
+    return format(number, 0);
+}
+
 function formatCoins(coins, element) {
     const platina = Math.floor(coins / 1e6)
     const gold = Math.floor((coins - platina * 1e6) / 1e4)
@@ -59,9 +66,24 @@ function formatCoins(coins, element) {
     }
 }
 
-function formatTime(sec_num, show_ms=false) {
+function formatTime(sec_num, show_ms = false) {
     if (sec_num == null) {
         return "unknown"
+    }
+    if (sec_num < 0) {
+        return '-' + formatTime(-sec_num, show_ms)
+    }
+    
+    if (sec_num >= 31536000) {
+        let years = Math.floor(sec_num / 31536000)
+        if (years >= 1000) {
+            return formatWhole(years) + ' years'
+        }
+        return years + 'y ' + formatTime(sec_num % 31536000, show_ms)
+    }
+    if (sec_num >= 86400) {
+        let days = Math.floor(sec_num / 86400)
+        return days + 'd ' + formatTime(sec_num % 86400, show_ms)
     }
 
     let hours = Math.floor(sec_num / 3600)
